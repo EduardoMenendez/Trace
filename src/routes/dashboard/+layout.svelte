155b3675710c33a6d4d8dd5622ca19f275c44346
EduardoMenendez <62720812+EduardoMenendez@users.noreply.github.com>
2025-03-18 12:57:43 -0600

    First Commit

---
+++ src/routes/dashboard/+layout.svelte
@@ -0,0 +1,16 @@
+<script>
+    import '../css/global.css';
+    let {children} = $props();
+    import Nav from '$lib/components/nav.svelte';
+</script>
+<div class="container">
+    <Nav/>
+    {@render children()}
+</div>
+<style>
+    .container{
+        display: flex;
+        gap: 30px;
+        height: 200vh;
+    }
+</style>
